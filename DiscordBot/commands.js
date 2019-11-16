6d30fd3a305e38ec242744f1ffa432e3e3fc6be4
Joe Wilkinson <joe.wilkinson@kpmg.co.uk>
2019-11-16 15:18:18 +0000

    Wrote bot to take inputs and process commands in a queue.

---
+++ DiscordBot/commands.js
@@ -0,0 +1,31 @@
+export const UP = new Promise((resolve,reject) => {
+    resolve('up');
+});
+
+export const DOWN = new Promise((resolve,reject) => {
+    resolve('down');
+});
+
+export const L = new Promise((resolve,reject) => {
+    resolve('left');
+});
+
+export const R = new Promise((resolve,reject) => {
+    resolve('right');
+});
+
+export const START = new Promise((resolve,reject) => {
+    resolve('start');
+});
+
+export const SELECT = new Promise((resolve,reject) => {
+    resolve('start');
+});
+
+export const A = new Promise((resolve,reject) => {
+    resolve('a');
+});
+
+export const B = new Promise((resolve,reject) => {
+    resolve('b');
+});
