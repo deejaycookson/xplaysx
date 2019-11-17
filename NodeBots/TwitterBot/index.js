105bece6b356a2984e3c04f3d1bffbb3087e2e52
Dan Cookson <dacookso@cisco.com>
2019-11-17 01:05:35 +0000

    changes

--- NodeBots/TwitterBot/index.js
+++ NodeBots/TwitterBot/index.js
@@ -1,25 +1,20 @@
-const Twitter = require('twit');
-const config = require('./config.js.js');
-const T = new Twitter(config);
+const http = require('request');
+const rp = require('request-promise');
+require('pipe').install()
 
-//let stream = T.stream('statuses/filter', { track: ['@xplaysx2'] });
-T.post('statuses/update', { status: 'API test!' }, (err, data, response) => {
-        console.log(err, data, response);
+let options = {
+    url: 'https://api.twitter.com/labs/1/tweets/stream/filter?format=detailed',
+    headers: {
+        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAACOXAwEAAAAAVi0rUyFJ892lwO27%2FvtBPNw7G9A%3DVmItgu7qrnTzmPyrN7R13ttNvDURPvT4rQMxQ8ZWaR6NQWGjZW'
     }
-);
-
-const tweetEvent = tweet => {
-
-    // Who sent the tweet?
-    var name = tweet.user.screen_name;
-    // What is the text?
-    // var txt = tweet.text;
-    // the status update or tweet ID in which we will reply
-    var nameID  = tweet.id_str;
-
-     // Get rid of the @ mention
-    // var txt = txt.replace(/@myTwitterHandle/g, "");
-    //console.log(tweet.text);
 };
 
-//stream.on('tweet', tweetEvent);
+http.get(options).on('response', (res) => {
+    res.on('data', function(chunk) {
+        console.log(chunk.toString());
+    });
+})
+
+function data(input){
+    
+}
